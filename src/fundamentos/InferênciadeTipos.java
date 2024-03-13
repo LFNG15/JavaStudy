57af4e182a9cf4724dbaef5d05a2ec2154539dbb
Luiz Felipe <luizfelipenascimentogomes@outlook.com>
2024-03-13 17:28:39 -0300

    Feat: DoWhile e InferênciadeTipos

---
+++ src/fundamentos/InferênciadeTipos.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class InferênciadeTipos {
+    public static void main(String[] args) {
+        double a = 4.5;
+        System.out.println(a);
+
+        var b = 4.5;
+        System.out.println(b);
+
+        var c = "Texto";
+        c = "Outro texto";
+        System.out.println(c);
+
+        double d;
+        d = 123.65;
+        System.out.println(d);
+        
+    }
+}
